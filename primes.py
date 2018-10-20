f13039763ac2c493e083bbbcd78f91581085d7dc
33marvil <martinramirezcruz71@gmail.com>
2018-10-20 13:44:13 -0500

    Is Prime TDD Python version 1

---
+++ primes.py
@@ -0,0 +1,28 @@
+# def is_prime(number):
+#     """Return True if *number* is prime."""
+#     # if number < 0: #
+#     #     return False
+#     #
+#     # if number in (0, 1):
+#     #     return False
+#     if number <= 1:
+#         return False
+#
+#     for element in range(2, number):
+#         if number % element == 0:
+#             return False
+#
+#     return True
+
+def is_prime(number):
+    """Return True if *number* is prime."""
+    if number <= 1:
+        return False
+
+    """Here refactor for loop"""
+    return False if len([False for element in range(2, number) if number % element == 0]) > 0  else True
+    # return ( number % element == 0 for element in range(2, number) )
+        # return False if number % element == 0 else True
+            # return False
+
+    # return True
